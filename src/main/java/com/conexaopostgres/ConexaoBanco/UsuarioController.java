9c12b62d3efa9a59eabe69bb18f2489df59123f4
JoaquimKayo <kayo_01@hotmail.com>
2021-12-13 23:02:05 -0300

    add cross origin *

--- src/main/java/com/conexaopostgres/ConexaoBanco/UsuarioController.java
+++ src/main/java/com/conexaopostgres/ConexaoBanco/UsuarioController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/usuario")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 
 	@Autowired
